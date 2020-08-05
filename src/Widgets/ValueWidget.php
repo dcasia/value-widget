24bc37670e3726a92b581412abb29275cbd857df
milewski <rafael.milewski@gmail.com>
2020-08-05 14:35:01 +0800

    update option format

--- src/Widgets/ValueWidget.php
+++ src/Widgets/ValueWidget.php
@@ -3,70 +3,64 @@
 namespace DigitalCreative\ValueWidget\Widgets;
 
 use DigitalCreative\NovaDashboard\Widget;
-use Illuminate\Support\Str;
+use DigitalCreative\NovaDashboard\WidgetOptionTab;
 use Laravel\Nova\Fields\KeyValue;
 use Laravel\Nova\Fields\Number;
 use Laravel\Nova\Fields\Text;
-use Laravel\Nova\Fields\Textarea;
 
 abstract class ValueWidget extends Widget
 {
 
-    public const OPTION_PREFIX = 'prefix';
-    public const OPTION_SUFFIX = 'suffix';
-    public const OPTION_TITLE = 'title';
-    public const OPTION_DECIMAL = 'decimal';
-    public const OPTION_NO_CURRENT_DATA = 'no_current_data';
-    public const OPTION_HELP = 'help';
-    public const OPTION_LABELS = 'labels';
+    public const PREFIX = 'prefix';
+    public const SUFFIX = 'suffix';
+    public const DECIMAL = 'decimal';
+    public const NO_CURRENT_DATA = 'no_current_data';
 
-    public const OPTION_LABEL_NO_PRIOR_DATA = 'No Prior Data';
-    public const OPTION_LABEL_NO_CURRENT_DATA = 'No Current Data';
-    public const OPTION_LABEL_NO_INCREASE = 'No Increase';
-    public const OPTION_LABEL_INCREASE = 'Increase';
-    public const OPTION_LABEL_DECREASE = 'Decrease';
-    public const OPTION_LABEL_PREVIOUS_VALUE = 'Previous Value';
+    public const LABELS = 'labels';
+    public const LABEL_NO_PRIOR_DATA = 'No Prior Data';
+    public const LABEL_NO_CURRENT_DATA = 'No Current Data';
+    public const LABEL_NO_INCREASE = 'No Increase';
+    public const LABEL_INCREASE = 'Increase';
+    public const LABEL_DECREASE = 'Decrease';
+    public const LABEL_PREVIOUS_VALUE = 'Previous Value';
 
     public function component(): string
     {
         return 'value-widget';
     }
 
-    public function fields(): array
+    public function widgetFields()
     {
-
-        return [
-
-            Text::make(__('Title'), static::OPTION_TITLE)->default(static function () {
-                return Str::title(Str::snake(class_basename(static::class), ' '));
-            }),
-
-            Text::make(__('Prefix'), static::OPTION_PREFIX),
-            Text::make(__('Suffix'), static::OPTION_SUFFIX),
-
-            Number::make(__('Decimal Rounding'), static::OPTION_DECIMAL)->default(2),
-
-            Text::make(__('No Current Data'), static::OPTION_NO_CURRENT_DATA)->default(__('N/A'))
-                ->help(__('Shown when there is no current data available.')),
-
-            KeyValue::make(__('Labels'), static::OPTION_LABELS)
-                    ->keyLabel(__('Option'))
-                    ->valueLabel(__('Label'))
-                    ->disableEditingKeys()
-                    ->disableDeletingRows()
-                    ->default([
-                        static::OPTION_LABEL_NO_PRIOR_DATA => __('No Prior Data'),
-                        static::OPTION_LABEL_NO_CURRENT_DATA => __('No Current Data'),
-                        static::OPTION_LABEL_NO_INCREASE => __('No Increase'),
-                        static::OPTION_LABEL_INCREASE => __('Increase'),
-                        static::OPTION_LABEL_DECREASE => __('Decrease'),
-                        static::OPTION_LABEL_PREVIOUS_VALUE => __('Previous value:')
-                    ]),
-
-            Textarea::make(__('Help'), static::OPTION_HELP)
-
-        ];
-
+        return WidgetOptionTab::make(__('Widget Settings'), [
+            self::PREFIX => static function (string $attribute) {
+                return Text::make(__('Prefix'), $attribute);
+            },
+            self::SUFFIX => static function (string $attribute) {
+                return Text::make(__('Suffix'), $attribute);
+            },
+            self::DECIMAL => static function (string $attribute) {
+                return Number::make(__('Decimal Rounding'), $attribute)->default(2);
+            },
+            self::NO_CURRENT_DATA => static function (string $attribute) {
+                return Text::make(__('No Current Data'), $attribute)->default(__('N/A'))
+                           ->help(__('Shown when there is no current data available.'));
+            },
+            self::LABELS => static function (string $attribute) {
+                return KeyValue::make(__('Labels'), $attribute)
+                               ->keyLabel(__('Option'))
+                               ->valueLabel(__('Label'))
+                               ->disableEditingKeys()
+                               ->disableDeletingRows()
+                               ->default([
+                                   static::LABEL_NO_PRIOR_DATA => __('No Prior Data'),
+                                   static::LABEL_NO_CURRENT_DATA => __('No Current Data'),
+                                   static::LABEL_NO_INCREASE => __('No Increase'),
+                                   static::LABEL_INCREASE => __('Increase'),
+                                   static::LABEL_DECREASE => __('Decrease'),
+                                   static::LABEL_PREVIOUS_VALUE => __('Previous value:'),
+                               ]);
+            },
+        ]);
     }
 
 }
